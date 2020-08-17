bead4e2e6951d94c3d18fdcf7900a1ebf0cc96e1
Wayne Chang <wyc@fastmail.fm>
2020-08-17 19:46:18 -0400

    Update DID Pest Grammar
    - Complete definitions from RFC3986 (URIs)
    - Add test that checks each high level parsed pair
    - Update project name to `ssi`

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,53 @@
 mod did;
 mod vc;
+extern crate pest;
+#[macro_use]
+extern crate pest_derive;
+
+use pest::Parser;
+use pest::error::Error;
+
+#[derive(Parser)]
+#[grammar = "did.pest"]
+struct DidParser;
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+
+    #[test]
+    fn parse_did_components() {
+        let input = "did:deadbeef:cafe/sub/path/?p1=v1&p2=v2#frag1";
+        let rv = DidParser::parse(Rule::did_url, input);
+        match rv {
+            Ok(pairs) => { 
+                //println!("{:#?}", pairs);
+                assert_eq!(input, pairs.as_str()); // ensure complete parsing
+                let mut pairs_iter = pairs;
+                let mut did_url_pairs_iter = pairs_iter.next().unwrap().into_inner();
+
+                // @TODO: check scheme subtokens (method_name, method_specific_id)
+                let did_scheme_pair = did_url_pairs_iter.next().unwrap();
+                assert_eq!(Rule::did_scheme, did_scheme_pair.as_rule());
+                assert_eq!("did:deadbeef:cafe", did_scheme_pair.as_str());
+
+                // @TODO: check path_abempty subtokens (segment*)
+                let path_abempty_pair = did_url_pairs_iter.next().unwrap();
+                assert_eq!(Rule::path_abempty, path_abempty_pair.as_rule());
+                assert_eq!("/sub/path/", path_abempty_pair.as_str());
+
+                let query_pair = did_url_pairs_iter.next().unwrap();
+                assert_eq!(Rule::query, query_pair.as_rule());
+                assert_eq!("p1=v1&p2=v2", query_pair.as_str());
+
+                let fragment_pair = did_url_pairs_iter.next().unwrap();
+                assert_eq!(Rule::fragment, fragment_pair.as_rule());
+                assert_eq!("frag1", fragment_pair.as_str());
+            },
+            Err(e) => panic!("error: {}", e),
+        }
+    }
+
     #[test]
     fn it_works() {
         assert_eq!(2 + 2, 4);
