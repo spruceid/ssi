fc82407abd8cf1455221ebe26722aa49991ca152
Charles E. Lehner <cel@celehner.com>
2020-08-19 17:10:15 -0400

    Test VCs/VPs
    Passes "Basic Documents" tests in vc-test-suite
    
    - Test credential construction from JSON
    - Validate during deserialization using TryFrom
    - Allow one or many context values
    - Allow object with id property for issuer
    - Allow multiple credential subjects
    - Allow multiple proofs
    - Allow multiple types in presentation
    - Expose did and vc modules

--- src/bin/ssi-vc-test/main.rs
+++ src/bin/ssi-vc-test/main.rs
@@ -1,15 +1,44 @@
+use ssi::vc::Contexts;
+use ssi::vc::Credential;
+use ssi::vc::Presentation;
+
 fn usage() {
     eprintln!("Usage: ssi-vc-test <generate|generate-presentation> <file>");
 }
 
-fn generate(data: &String) -> &String {
-    // TODO
-    return data;
+fn generate(data: String) -> String {
+    let doc: Credential = serde_json::from_str(&data).unwrap();
+    if !doc.type_.contains(&"VerifiableCredential".to_string()) {
+        panic!("Missing type VerifiableCredential");
+    }
+
+    // work around https://github.com/w3c/vc-test-suite/issues/96
+    if doc.type_.len() > 1 {
+        if let Contexts::Many(ref context) = doc.context {
+            if context.len() == 1 {
+                panic!("If there are multiple types, there should be multiple contexts.");
+            }
+        }
+    }
+
+    // @TODO: sign/verify
+    return serde_json::to_string_pretty(&doc).unwrap();
 }
 
-fn generate_presentation(data: &String) -> &String {
-    // TODO
-    return data;
+fn generate_presentation(data: String) -> String {
+    let doc: Presentation = serde_json::from_str(&data).unwrap();
+    if !doc.type_.contains(&"VerifiablePresentation".to_string()) {
+        panic!("Missing type VerifiablePresentation");
+    }
+
+    // note: for JWT, proof may be outside the VC object
+    if !doc.proof.is_some() {
+        panic!("Missing proof");
+    }
+
+    // @TODO: sign/verify
+    let response = serde_json::to_string_pretty(&doc).unwrap();
+    return response;
 }
 
 fn read_json(filename: &String) -> String {
@@ -28,7 +57,7 @@ fn read_json(filename: &String) -> String {
     data
 }
 
-fn write_json(data: &String) {
+fn write_json(data: String) {
     use std::io::Write;
     let stdout = std::io::stdout();
     let mut handle = stdout.lock();
@@ -48,13 +77,13 @@ fn main() {
     match &cmd[..] {
         "generate" => {
             let data: String = read_json(&filename);
-            let output: &String = generate(&data);
-            write_json(&output);
+            let output: String = generate(data);
+            write_json(output);
         }
         "generate-presentation" => {
             let data: String = read_json(&filename);
-            let output: &String = generate_presentation(&data);
-            write_json(&output);
+            let output: String = generate_presentation(data);
+            write_json(output);
         }
         _ => {
             eprintln!("Unexpected command '{}'", cmd);
--- src/vc.rs
+++ src/vc.rs
@@ -1,9 +1,10 @@
 use std::collections::HashMap as Map;
+use std::convert::TryFrom;
 
-use serde::{Serialize, Deserialize};
-use serde_json;
-use serde_json::{Value, json};
 use chrono::prelude::*;
+use serde::{Deserialize, Serialize};
+use serde_json;
+use serde_json::{json, Value};
 
 // ********************************************
 // * Data Structures for Verifiable Credentials
@@ -13,23 +14,24 @@ use chrono::prelude::*;
 // @TODO items:
 // - `id` fields must all be URIs
 
+pub const DEFAULT_CONTEXT: &str = "https://www.w3.org/2018/credentials/v1";
+
 #[derive(Debug, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct Credential {
     #[serde(rename = "@context")]
-    pub context: Vec<String>,
+    pub context: Contexts,
     pub id: String,
     #[serde(rename = "type")]
     pub type_: Vec<String>,
-    pub credential_subject: Subject,
-    // @TODO issuer can also be an object with an `id` property
-    pub issuer: String,
+    pub credential_subject: Subjects,
+    pub issuer: Issuer,
     pub issuance_date: DateTime<Utc>, // must be RFC3339
     // This field is populated only when using
     // embedded proofs such as LD-PROOF
     //   https://w3c-ccg.github.io/ld-proofs/
     #[serde(skip_serializing_if = "Option::is_none")]
-    pub proof: Option<Proof>,
+    pub proof: Option<Proofs>,
     #[serde(skip_serializing_if = "Option::is_none")]
     pub expiration_date: Option<DateTime<Utc>>, // must be RFC3339
     #[serde(skip_serializing_if = "Option::is_none")]
@@ -41,6 +43,35 @@ pub struct Credential {
 }
 
 #[derive(Debug, Serialize, Deserialize)]
+#[serde(untagged)]
+#[serde(try_from = "ContextsUnchecked")]
+pub enum Contexts {
+    One(Context),
+    Many(Vec<Context>),
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(untagged)]
+pub enum ContextsUnchecked {
+    One(Context),
+    Many(Vec<Context>),
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(untagged)]
+pub enum Context {
+    URI(String),
+    Object(Map<String, Value>),
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(untagged)]
+pub enum Subjects {
+    One(Subject),
+    Many(Vec<Subject>),
+}
+
+#[derive(Debug, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct Subject {
     #[serde(skip_serializing_if = "Option::is_none")]
@@ -48,6 +79,37 @@ pub struct Subject {
     #[serde(skip_serializing_if = "Option::is_none")]
     #[serde(flatten)]
     pub property_set: Option<Map<String, Value>>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(untagged)]
+#[serde(try_from = "IssuerUnchecked")]
+pub enum Issuer {
+    URI(String),
+    Object(ObjectWithId),
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(untagged)]
+pub enum IssuerUnchecked {
+    URI(String),
+    Object(ObjectWithId),
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct ObjectWithId {
+    pub id: String,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    #[serde(flatten)]
+    pub property_set: Option<Map<String, Value>>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(untagged)]
+pub enum Proofs {
+    One(Proof),
+    Many(Vec<Proof>),
 }
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -95,13 +157,136 @@ pub struct Presentation {
     #[serde(rename = "@context")]
     pub context: Vec<String>,
     #[serde(rename = "type")]
-    pub type_: String,
+    pub type_: Vec<String>,
     pub verifiable_credential: Vec<Credential>,
     // This field is populated only when using
     // embedded proofs such as LD-PROOF
     //   https://w3c-ccg.github.io/ld-proofs/
     #[serde(skip_serializing_if = "Option::is_none")]
-    pub proof: Option<Proof>,
+    pub proof: Option<Proofs>,
     #[serde(skip_serializing_if = "Option::is_none")]
     pub holder: Option<String>,
+}
+
+impl TryFrom<ContextsUnchecked> for Contexts {
+    type Error = &'static str;
+    fn try_from(context: ContextsUnchecked) -> Result<Self, Self::Error> {
+        // first context must be the default
+        match context {
+            // @TODO: make more DRY
+            ContextsUnchecked::One(context) => match context {
+                Context::URI(uri) => {
+                    if uri != DEFAULT_CONTEXT.to_string() {
+                        Err("Invalid context")
+                    } else {
+                        Ok(Contexts::One(Context::URI(uri)))
+                    }
+                }
+                Context::Object(_) => Err("Base context must be URI"),
+            },
+            ContextsUnchecked::Many(contexts) => {
+                if contexts.len() == 0 {
+                    Err("Missing context")
+                } else {
+                    let first_context = &contexts[0];
+                    match first_context {
+                        Context::URI(uri) => {
+                            if uri != DEFAULT_CONTEXT {
+                                return Err("Invalid context");
+                            }
+                            // @TODO: check strings are URIs and objects are valid
+                            // context definitions
+                            Ok(Contexts::Many(contexts))
+                        }
+                        Context::Object(_) => Err("Base context must be URI"),
+                    }
+                }
+            }
+        }
+    }
+}
+
+impl TryFrom<IssuerUnchecked> for Issuer {
+    type Error = &'static str;
+    fn try_from(issuer: IssuerUnchecked) -> Result<Self, Self::Error> {
+        // must be either URI or object containing id property
+        match issuer {
+            IssuerUnchecked::URI(uri) => {
+                // @TODO: more complete checking
+                if uri.contains(":") {
+                    Ok(Issuer::URI(uri))
+                } else {
+                    Err("Issuer string is not a URI")
+                }
+            }
+            IssuerUnchecked::Object(object) => {
+                // id property is already required by the ObjectWithId struct
+                Ok(Issuer::Object(object))
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn credential_from_json() {
+        let doc_str = "{\
+            \"@context\": \"https://www.w3.org/2018/credentials/v1\",\
+            \"id\": \"http://example.org/credentials/3731\",\
+            \"type\": [\"VerifiableCredential\"],\
+            \"issuer\": \"did:example:30e07a529f32d234f6181736bd3\",\
+            \"issuanceDate\": \"2020-08-19T21:41:50Z\",\
+            \"credentialSubject\": {\
+                \"id\": \"did:example:d23dd687a7dc6787646f2eb98d0\"\
+            }\
+        }";
+        let id = "http://example.org/credentials/3731";
+        let doc: Credential = serde_json::from_str(doc_str).unwrap();
+        println!("{}", serde_json::to_string_pretty(&doc).unwrap());
+        assert_eq!(doc.id, id);
+    }
+
+    #[test]
+    fn credential_multiple_contexts() {
+        let doc_str = "{\
+            \"@context\": [\
+              \"https://www.w3.org/2018/credentials/v1\",\
+              \"https://www.w3.org/2018/credentials/examples/v1\"\
+            ],\
+            \"id\": \"http://example.org/credentials/3731\",\
+            \"type\": [\"VerifiableCredential\"],\
+            \"issuer\": \"did:example:30e07a529f32d234f6181736bd3\",\
+            \"issuanceDate\": \"2020-08-19T21:41:50Z\",\
+            \"credentialSubject\": {\
+                \"id\": \"did:example:d23dd687a7dc6787646f2eb98d0\"\
+            }\
+        }";
+        let doc: Credential = serde_json::from_str(doc_str).unwrap();
+        println!("{}", serde_json::to_string_pretty(&doc).unwrap());
+        if let Contexts::Many(contexts) = doc.context {
+            assert_eq!(contexts.len(), 2);
+        } else {
+            assert!(false);
+        }
+    }
+
+    #[test]
+    #[should_panic(expected = "Invalid context")]
+    fn credential_invalid_context() {
+        let doc_str = "{\
+            \"@context\": \"https://example.org/invalid-context\",
+            \"id\": \"http://example.org/credentials/3731\",\
+            \"type\": [\"VerifiableCredential\"],\
+            \"issuer\": \"did:example:30e07a529f32d234f6181736bd3\",\
+            \"issuanceDate\": \"2020-08-19T21:41:50Z\",\
+            \"credentialSubject\": {\
+                \"id\": \"did:example:d23dd687a7dc6787646f2eb98d0\"\
+            }\
+        }";
+        let doc: Credential = serde_json::from_str(doc_str).unwrap();
+        println!("{}", serde_json::to_string_pretty(&doc).unwrap());
+    }
 }
