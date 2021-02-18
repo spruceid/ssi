3e9d3e860a4e1dab3ed20cd4a1241977fdbea088
Charles E. Lehner <charles.lehner@spruceid.com>
2021-02-18 13:54:56 -0500

    Add missing number 8

--- src/rdf.rs
+++ src/rdf.rs
@@ -205,7 +205,7 @@ fn parse_lang_subtag(chars: &mut Peekable<Chars>, string: &mut String) -> Result
             | 'o' | 'p' | 'q' | 'r' | 's' | 't' | 'u' | 'v' | 'w' | 'x' | 'y' | 'z' | 'A' | 'B'
             | 'C' | 'D' | 'E' | 'F' | 'G' | 'H' | 'I' | 'J' | 'K' | 'L' | 'M' | 'N' | 'O' | 'P'
             | 'Q' | 'R' | 'S' | 'T' | 'U' | 'V' | 'W' | 'X' | 'Y' | 'Z' | '0' | '1' | '2' | '3'
-            | '4' | '5' | '6' | '7' | '9' => {
+            | '4' | '5' | '6' | '7' | '8' | '9' => {
                 string.push(c);
             }
             ' ' | '\t' => break,
