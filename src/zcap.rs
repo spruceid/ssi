84c66e9932f64eee53ee5d49bdf6eb7bfc1f9dc9
Charles Cunningham <c.a.cunningham6@gmail.com>
2021-06-04 16:05:53 +0200

    add basic zcap deser test

--- src/zcap.rs
+++ src/zcap.rs
@@ -1,4 +1,5 @@
-use std::collections::HashMap;
+use std::collections::HashMap as Map;
+use std::convert::TryFrom;
 
 use crate::did_resolve::{DIDResolver, ResolutionInputMetadata};
 use crate::error::Error;
@@ -7,13 +8,13 @@ use crate::jwk::{JWTKeys, JWK};
 use crate::ldp::{now_ms, LinkedDataDocument, LinkedDataProofs, ProofPreparation};
 use crate::one_or_many::OneOrMany;
 use crate::rdf::DataSet;
-use crate::vc::{Check, Contexts, LinkedDataProofOptions, Proof, VerificationResult, URI};
+use crate::vc::{Check, LinkedDataProofOptions, Proof, VerificationResult, URI};
 
 use async_trait::async_trait;
 use serde::{Deserialize, Serialize};
 use serde_json::Value;
 
-const DEFAULT_CONTEXT: &str = "https://www.w3.org/security/v2";
+const DEFAULT_CONTEXT: &str = "https://w3id.org/security/v2";
 
 // limited initial definition of a ZCAP, generic over Action and Caveat types
 #[derive(Debug, Serialize, Deserialize, Clone)]
@@ -24,7 +25,7 @@ pub struct ZCAP<A, C> {
     pub id: URI,
     pub parent_capability: URI,
     #[serde(skip_serializing_if = "Option::is_none")]
-    pub invoker: Option<String>,
+    pub invoker: Option<URI>,
     #[serde(skip_serializing_if = "Option::is_none")]
     pub action: Option<A>,
     #[serde(skip_serializing_if = "Option::is_none")]
@@ -36,7 +37,7 @@ pub struct ZCAP<A, C> {
     pub proof: Option<OneOrMany<Proof>>,
     #[serde(skip_serializing_if = "Option::is_none")]
     #[serde(flatten)]
-    pub property_set: Option<HashMap<String, Value>>,
+    pub property_set: Option<Map<String, Value>>,
 }
 
 impl<A, C> ZCAP<A, C>
@@ -91,5 +92,76 @@ where
         };
         let mut loader = StaticLoader;
         json_to_dataset(&json, more_contexts.as_ref(), false, None, &mut loader).await
+    }
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone, PartialEq)]
+#[serde(untagged)]
+#[serde(try_from = "OneOrMany<Context>")]
+pub enum Contexts {
+    One(Context),
+    Many(Vec<Context>),
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone, PartialEq)]
+#[serde(untagged)]
+pub enum Context {
+    URI(URI),
+    Object(Map<String, Value>),
+}
+
+impl TryFrom<OneOrMany<Context>> for Contexts {
+    type Error = Error;
+    fn try_from(context: OneOrMany<Context>) -> Result<Self, Self::Error> {
+        let first_uri = match context.first() {
+            None => return Err(Error::MissingContext),
+            Some(Context::URI(URI::String(uri))) => uri,
+            Some(Context::Object(_)) => return Err(Error::InvalidContext),
+        };
+        if first_uri != DEFAULT_CONTEXT {
+            return Err(Error::InvalidContext);
+        }
+        Ok(match context {
+            OneOrMany::One(context) => Contexts::One(context),
+            OneOrMany::Many(contexts) => Contexts::Many(contexts),
+        })
+    }
+}
+
+impl From<Contexts> for OneOrMany<Context> {
+    fn from(contexts: Contexts) -> OneOrMany<Context> {
+        match contexts {
+            Contexts::One(context) => OneOrMany::One(context),
+            Contexts::Many(contexts) => OneOrMany::Many(contexts),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn zcap_from_json() {
+        let zcap_str = include_str!("../examples/zcap.jsonld");
+        let zcap: ZCAP<(), ()> = serde_json::from_str(zcap_str).unwrap();
+        assert_eq!(
+            zcap.context,
+            Contexts::One(Context::URI(URI::String(DEFAULT_CONTEXT.into())))
+        );
+        assert_eq!(
+            zcap.id,
+            URI::String("https://whatacar.example/a-fancy-car/proc/7a397d7b".into())
+        );
+        assert_eq!(
+            zcap.parent_capability,
+            URI::String("https://whatacar.example/a-fancy-car".into())
+        );
+        assert_eq!(
+            zcap.invoker,
+            Some(URI::String(
+                "https://social.example/alyssa#key-for-car".into()
+            ))
+        );
     }
 }
