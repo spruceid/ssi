a7c8e9ebb0132ef6dc27893b8b6881a72996e483
Charles E. Lehner <charles.lehner@spruceid.com>
2021-06-24 11:32:03 -0400

    Fix use of p256 in did-key

--- did-key/src/lib.rs
+++ did-key/src/lib.rs
@@ -270,7 +270,7 @@ impl DIDMethod for DIDKey {
                     "P-256" => {
                         use p256::elliptic_curve::sec1::ToEncodedPoint;
                         use std::convert::TryFrom;
-                        let pk = match k256::PublicKey::try_from(params) {
+                        let pk = match p256::PublicKey::try_from(params) {
                             Ok(pk) => pk,
                             Err(_err) => return None,
                         };
