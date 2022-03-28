7db234b64a3ea3c117fb4d37c770ed9d2009a4f3
Victor Dods <victor.dods@gmail.com>
2022-03-28 00:10:06 -0700

    Added a crucial, missing conversion -- ssi::vc::VCDateTime into chrono::DateTime<Tz>.

--- src/vc.rs
+++ src/vc.rs
@@ -729,6 +729,15 @@ where
     }
 }
 
+impl<Tz: chrono::TimeZone> Into<DateTime<Tz>> for VCDateTime
+where
+    chrono::DateTime<chrono::FixedOffset>: Into<chrono::DateTime<Tz>>,
+{
+    fn into(self) -> DateTime<Tz> {
+        self.date_time.into()
+    }
+}
+
 pub fn base64_encode_json<T: Serialize>(object: &T) -> Result<String, Error> {
     let json = serde_json::to_string(&object)?;
     Ok(base64::encode_config(json, base64::URL_SAFE_NO_PAD))
@@ -2465,6 +2474,14 @@ pub(crate) mod tests {
         }"###;
         let doc: Credential = serde_json::from_str(doc_str).unwrap();
         println!("{}", serde_json::to_string_pretty(&doc).unwrap());
+    }
+
+    #[test]
+    fn test_vc_date_time_roundtrip() {
+        let expected_utc_now = chrono::Utc::now();
+        let vc_date_time_now = VCDateTime::from(expected_utc_now);
+        let roundtripped_utc_now: chrono::DateTime::<chrono::Utc> = vc_date_time_now.into();
+        assert_eq!(roundtripped_utc_now, expected_utc_now);
     }
 
     #[async_std::test]
