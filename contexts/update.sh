b4993a9635c74cdf7ea0d83d9c5090ab4f8303e5
Charles E. Lehner <charles.lehner@spruceid.com>
2022-05-06 09:44:32 -0400

    Fix rl2020 context filename in update script

--- contexts/update.sh
+++ contexts/update.sh
@@ -25,6 +25,6 @@ exec curl \
 	https://w3id.org/vdl/v1 -o w3id-vdl-v1.jsonld \
 	https://w3id.org/wallet/v1 -o w3id-wallet-v1.jsonld \
 	https://w3id.org/zcap/v1 -o w3id-zcap-v1.jsonld \
-	https://w3id.org/vc-revocation-list-2020/v1 -o w3id-vc-revocation-list-2020-v1.json \
+	https://w3id.org/vc-revocation-list-2020/v1 -o w3id-vc-revocation-list-2020-v1.jsonld \
 	https://w3id.org/vc/status-list/2021/v1 -o w3id-vc-status-list-2021-v1.jsonld \
 	-L
