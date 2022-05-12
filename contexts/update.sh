bafe7a5576051ff990e57c40ad1b9e7b7b208b1d
Charles E. Lehner <charles.lehner@spruceid.com>
2022-05-12 12:24:18 -0400

    Remove esrs2020-extra from contexts updater

--- contexts/update.sh
+++ contexts/update.sh
@@ -19,7 +19,6 @@ exec curl \
 	https://w3id.org/citizenship/v1 -o w3c-ccg-citizenship-v1.jsonld \
 	https://w3id.org/vaccination/v1 -o w3c-ccg-vaccination-v1.jsonld \
 	https://w3id.org/traceability/v1 -o w3c-ccg-traceability-v1.jsonld \
-	https://demo.spruceid.com/EcdsaSecp256k1RecoverySignature2020/esrs2020-extra-0.0.jsonld -o esrs2020-extra-0.0.jsonld \
 	https://w3id.org/security/bbs/v1 -o bbs-v1.jsonld \
 	https://identity.foundation/presentation-exchange/submission/v1 -o presentation-submission.jsonld \
 	https://w3id.org/vdl/v1 -o w3id-vdl-v1.jsonld \
