45bdde14f9c3d969a334f94645a0d00547ae082d
Charles Cunningham <charlescunningham@Charless-MacBook-Pro.fritz.box>
2022-06-02 18:19:12 +0200

    remove unnecessary parts and add verification and signing

--- src/ucan.rs
+++ src/ucan.rs
@@ -1,33 +1,13 @@
-use crate::vc::URI;
-use serde::{Deserialize, Serialize};
+use crate::{
+    error::Error,
+    jwk::{Algorithm, JWK},
+    jws::{encode_sign_custom_header, Header},
+    jwt::decode_verify,
+    vc::{NumericDate, URI},
+};
+use serde::{de::DeserializeOwned, Deserialize, Serialize};
 use serde_json::Value as JsonValue;
-use std::{collections::HashMap, convert::TryFrom};
-
-pub struct UCAN<S = Vec<u8>, F = JsonValue, A = HashMap<String, JsonValue>> {
-    pub header: Header,
-    pub payload: Payload<F, A>,
-    pub signature: S,
-    // to preserve exact signed bytes in case of out-of-order
-    // fields when deserialising
-    // ugh setting this during "b64_encode" will require &mut self :(
-    // signed_data: Vec<u8>,
-}
-
-impl<S, F, A> UCAN<S, F, A>
-where
-    S: AsRef<[u8]>,
-    F: Serialize,
-    A: Serialize,
-{
-    pub fn b64_encode(&self) -> Result<String, serde_json::Error> {
-        Ok([
-            b64encode(&serde_json::to_vec(&self.header)?),
-            b64encode(&serde_json::to_vec(&self.payload)?),
-            b64encode(&self.signature),
-        ]
-        .join("."))
-    }
-}
+use std::collections::HashMap;
 
 #[derive(thiserror::Error, Debug)]
 pub enum DecodeError<E> {
@@ -41,33 +21,6 @@ pub enum DecodeError<E> {
     Signature(E),
 }
 
-impl<'de, S, F, A> UCAN<S, F, A>
-where
-    S: TryFrom<Vec<u8>>,
-    F: Deserialize<'de>,
-    A: Deserialize<'de>,
-{
-    pub fn b64_decode(b64: &str) -> Result<Self, DecodeError<S::Error>> {
-        use serde_json::from_slice;
-
-        let mut parts = b64.split('.').map(b64decode);
-        let header = from_slice(&parts.next().transpose()?.ok_or(DecodeError::Form)?)?;
-        let payload = from_slice(&parts.next().transpose()?.ok_or(DecodeError::Form)?)?;
-        let signature = S::try_from(parts.next().transpose()?.ok_or(DecodeError::Form)?)
-            .map_err(DecodeError::Signature)?;
-
-        if parts.next() != None {
-            Err(DecodeError::Form)
-        } else {
-            Ok(Self {
-                header,
-                payload,
-                signature,
-            })
-        }
-    }
-}
-
 #[derive(Serialize, Deserialize, Clone)]
 pub struct Payload<F = JsonValue, A = HashMap<String, JsonValue>> {
     #[serde(rename = "iss")]
@@ -75,9 +28,9 @@ pub struct Payload<F = JsonValue, A = HashMap<String, JsonValue>> {
     #[serde(rename = "aud")]
     pub audience: String,
     #[serde(rename = "nbf", skip_serializing_if = "Option::is_none")]
-    pub not_before: Option<u64>,
+    pub not_before: Option<NumericDate>,
     #[serde(rename = "exp")]
-    pub expiration: u64,
+    pub expiration: NumericDate,
     #[serde(rename = "nnc", skip_serializing_if = "Option::is_none")]
     pub nonce: Option<String>,
     #[serde(rename = "fct", skip_serializing_if = "Option::is_none")]
@@ -97,13 +50,47 @@ pub enum TimeInvalid {
 }
 
 impl<F, A> Payload<F, A> {
-    pub fn validate_time(&self, time: Option<u64>) -> Result<(), TimeInvalid> {
+    pub fn validate_time(&self, time: Option<f64>) -> Result<(), TimeInvalid> {
         let t = time.unwrap_or_else(now);
-        match (self.not_before, t > self.expiration) {
+        match (self.not_before, t > self.expiration.as_seconds()) {
             (_, true) => Err(TimeInvalid::TooLate),
-            (Some(nbf), _) if t < nbf => Err(TimeInvalid::TooEarly),
+            (Some(nbf), _) if t < nbf.as_seconds() => Err(TimeInvalid::TooEarly),
             _ => Ok(()),
         }
+    }
+
+    // NOTE IntoIter::new is deprecated, but into_iter() returns references until we move to 2021 edition
+    #[allow(deprecated)]
+    pub fn encode_sign(&self, algorithm: Algorithm, key: &JWK) -> Result<String, Error>
+    where
+        F: Serialize,
+        A: Serialize,
+    {
+        encode_sign_custom_header(
+            &serde_json::to_string(&self)?,
+            key,
+            &Header {
+                algorithm,
+                key_id: key.key_id.clone(),
+                type_: Some("JWT".to_string()),
+                additional_parameters: std::array::IntoIter::new([(
+                    "ucv".to_string(),
+                    serde_json::Value::String("0.8.1".to_string()),
+                )])
+                .collect(),
+                ..Default::default()
+            },
+        )
+    }
+
+    pub fn decode_verify(jwt: &str, key: &JWK) -> Result<Self, Error>
+    where
+        F: DeserializeOwned,
+        A: DeserializeOwned,
+    {
+        let p = decode_verify(jwt, key)?;
+        // TODO check that the issuing key matches/is part of the DID Doc
+        Ok(p)
     }
 }
 
@@ -117,53 +104,7 @@ pub struct Capability<A = HashMap<String, JsonValue>> {
     pub additional_fields: Option<A>,
 }
 
-/// 3.1 Header
-/// The header is a standard JWT header, with an additional REQUIRED field ucv.
-/// This field sets the version of the UCAN specification used in the payload.
-#[derive(Serialize, Deserialize, Clone)]
-pub struct Header {
-    #[serde(rename = "alg")]
-    algorithm: String,
-    typ: Typ,
-    ucv: UCV,
-}
-
-impl Header {
-    pub fn new(alg: &str) -> Self {
-        Self {
-            algorithm: alg.to_string(),
-            typ: Typ::JWT,
-            ucv: UCV::V0_8_1,
-        }
-    }
-    pub fn version(&self) -> &str {
-        "0.8.1"
-    }
-    pub fn algorithm(&self) -> &str {
-        &self.algorithm
-    }
-}
-
-#[derive(Serialize, Deserialize, Clone)]
-enum Typ {
-    JWT,
-}
-
-#[derive(Serialize, Deserialize, Clone)]
-enum UCV {
-    #[serde(rename = "0.8.1")]
-    V0_8_1,
-}
-
-fn b64encode<D: AsRef<[u8]>>(d: &D) -> String {
-    base64::encode_config(d.as_ref(), base64::URL_SAFE_NO_PAD)
-}
-
-fn b64decode(s: &str) -> Result<Vec<u8>, base64::DecodeError> {
-    base64::decode_config(s, base64::URL_SAFE_NO_PAD)
-}
-
-fn now() -> u64 {
+fn now() -> f64 {
     #[cfg(target_arch = "wasm32")]
     use instant::SystemTime;
     #[cfg(not(target_arch = "wasm32"))]
@@ -172,14 +113,5 @@ fn now() -> u64 {
     SystemTime::now()
         .duration_since(SystemTime::UNIX_EPOCH)
         .unwrap()
-        .as_secs()
-}
-
-#[test]
-fn header() {
-    let h = Header::new("alg");
-    assert_eq!(
-        serde_json::to_string(&h).unwrap(),
-        r#"{"alg":"alg","typ":"JWT","ucv":"0.8.1"}"#
-    );
+        .as_secs() as f64
 }
