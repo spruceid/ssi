7693f03c8dcc81b73c1b16bb8e7cedd8d5ad572e
Charles Cunningham <charlescunningham@Charless-MacBook-Pro.fritz.box>
2022-06-08 12:20:47 +0200

    add parent extraction and verification from did with opt fragment

--- src/ucan.rs
+++ src/ucan.rs
@@ -1,13 +1,120 @@
 use crate::{
+    did::{Resource, VerificationMethod},
+    did_resolve::{dereference, Content, DIDResolver},
     error::Error,
     jwk::{Algorithm, JWK},
-    jws::{encode_sign_custom_header, Header},
+    jws::{decode_jws_parts, encode_sign_custom_header, split_jws, verify_bytes, Header},
     jwt::decode_verify,
     vc::{NumericDate, URI},
 };
+use futures::future::try_join_all;
 use serde::{de::DeserializeOwned, Deserialize, Serialize};
 use serde_json::Value as JsonValue;
 use std::collections::HashMap;
+
+#[derive(Clone)]
+pub struct Ucan<F = JsonValue, A = HashMap<String, JsonValue>> {
+    pub header: Header,
+    pub payload: Payload<F, A>,
+    pub signature: Vec<u8>,
+}
+
+#[derive(Clone)]
+pub struct DecodedUcanTree<F = JsonValue, A = HashMap<String, JsonValue>> {
+    pub ucan: Ucan<F, A>,
+    pub parents: Vec<DecodedUcanTree<F, A>>,
+}
+
+impl<F, A> Ucan<F, A> {
+    pub async fn decode_verify(
+        jwt: &str,
+        resolver: &dyn DIDResolver,
+    ) -> Result<DecodedUcanTree<F, A>, Error>
+    where
+        F: DeserializeOwned,
+        A: DeserializeOwned,
+    {
+        let parts = split_jws(jwt).and_then(|(h, p, s)| decode_jws_parts(h, p.as_bytes(), s))?;
+        let payload: Payload<F, A> = serde_json::from_slice(&parts.payload)?;
+
+        // extract or deduce signing key
+        let key: &JWK = match (
+            payload.issuer.split(':').nth(1),
+            &parts.header.jwk,
+            dereference(resolver, &payload.issuer, &Default::default())
+                .await
+                .1,
+        ) {
+            // did:pkh with and without fragment
+            (Some("pkh"), Some(jwk), Content::DIDDocument(_) | Content::Object(_)) => jwk,
+            // did:key without fragment
+            (Some("key"), _, Content::DIDDocument(d)) => &d
+                .verification_method
+                .iter()
+                .flatten()
+                .next()
+                .and_then(|v| match v {
+                    VerificationMethod::Map(vm) => Some(vm),
+                    _ => None,
+                })
+                .ok_or_else(|| Error::VerificationMethodMismatch)?
+                .get_jwk()?,
+            // general case, did with fragment
+            (Some(_), _, Content::Object(Resource::VerificationMethod(vm))) => &vm.get_jwk()?,
+            _ => return Err(Error::VerificationMethodMismatch),
+        };
+
+        verify_bytes(
+            parts.header.algorithm,
+            &parts.signing_input,
+            &key,
+            &parts.signature,
+        )?;
+
+        Ok(DecodedUcanTree {
+            // decode and verify parents
+            parents: try_join_all(
+                payload
+                    .proof
+                    .iter()
+                    .map(|s| Self::decode_verify(s, resolver)),
+            )
+            .await?,
+            ucan: Ucan {
+                header: parts.header,
+                payload,
+                signature: parts.signature,
+            },
+        })
+    }
+
+    pub fn decode(jwt: &str) -> Result<Self, Error>
+    where
+        F: DeserializeOwned,
+        A: DeserializeOwned,
+    {
+        let parts = split_jws(jwt).and_then(|(h, p, s)| decode_jws_parts(h, p.as_bytes(), s))?;
+        let payload: Payload<F, A> = serde_json::from_slice(&parts.payload)?;
+        Ok(Self {
+            header: parts.header,
+            payload,
+            signature: parts.signature,
+        })
+    }
+
+    pub fn parents(&self) -> ParentIter {
+        self.payload.parents()
+    }
+}
+
+pub struct ParentIter<'a>(std::slice::Iter<'a, String>);
+
+impl<'a> Iterator for ParentIter<'a> {
+    type Item = Result<Ucan, Error>;
+    fn next(&mut self) -> Option<Result<Ucan, Error>> {
+        self.0.next().map(|s| Ucan::decode(s))
+    }
+}
 
 #[derive(thiserror::Error, Debug)]
 pub enum DecodeError<E> {
@@ -90,7 +197,13 @@ impl<F, A> Payload<F, A> {
     {
         let p = decode_verify(jwt, key)?;
         // TODO check that the issuing key matches/is part of the DID Doc
+
+        // TODO check parents!! we need to extract the issuing key somehow from them
         Ok(p)
+    }
+
+    pub fn parents(&self) -> ParentIter {
+        ParentIter(self.payload.proof.iter())
     }
 }
 
@@ -113,5 +226,5 @@ fn now() -> f64 {
     SystemTime::now()
         .duration_since(SystemTime::UNIX_EPOCH)
         .unwrap()
-        .as_secs() as f64
+        .as_secs_f64()
 }
