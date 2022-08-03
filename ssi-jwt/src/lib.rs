b767e2be051daca3bc04d565fb9a1a94c9cfab9d
Charles Cunningham <charlescunningham@Charless-MBP.fritz.box>
2022-08-03 13:17:54 +0200

    move jwt to ssi-jwt crate

--- ssi-jwt/src/lib.rs
+++ ssi-jwt/src/lib.rs
@@ -1,8 +1,8 @@
 use serde::de::DeserializeOwned;
 use serde::Serialize;
 
-use crate::error::Error;
-use crate::jwk::{Algorithm, JWK};
+use ssi_jwk::{Algorithm, JWK};
+use ssi_jws::Error;
 
 // RFC 7519 - JSON Web Token (JWT)
 
@@ -12,23 +12,23 @@ pub fn encode_sign<Claims: Serialize>(
     key: &JWK,
 ) -> Result<String, Error> {
     let payload = serde_json::to_string(claims)?;
-    crate::jws::encode_sign(algorithm, &payload, key)
+    ssi_jws::encode_sign(algorithm, &payload, key)
 }
 
 pub fn encode_unsigned<Claims: Serialize>(claims: &Claims) -> Result<String, Error> {
     let payload = serde_json::to_string(claims)?;
-    crate::jws::encode_unsigned(&payload)
+    ssi_jws::encode_unsigned(&payload)
 }
 
 pub fn decode_verify<Claims: DeserializeOwned>(jwt: &str, key: &JWK) -> Result<Claims, Error> {
-    let (_header, payload) = crate::jws::decode_verify(jwt, key)?;
+    let (_header, payload) = ssi_jws::decode_verify(jwt, key)?;
     let claims = serde_json::from_slice(&payload)?;
     Ok(claims)
 }
 
 // for vc-test-suite
 pub fn decode_unverified<Claims: DeserializeOwned>(jwt: &str) -> Result<Claims, Error> {
-    let (_header, payload) = crate::jws::decode_unverified(jwt)?;
+    let (_header, payload) = ssi_jws::decode_unverified(jwt)?;
     let claims = serde_json::from_slice(&payload)?;
     Ok(claims)
 }
