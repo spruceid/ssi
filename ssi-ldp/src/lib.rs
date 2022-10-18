05805763d6d9ebec6c073bcd67c021b8bc7d79a2
chunningham <c.a.cunningham6@gmail.com>
2022-10-18 11:35:18 +0200

    fix default proof type selection (#475)
    * ensure/pick relationship before choosing suite in signing and preparation

--- ssi-ldp/src/lib.rs
+++ ssi-ldp/src/lib.rs
@@ -469,6 +469,8 @@ impl LinkedDataProofs {
         key: &JWK,
         extra_proof_properties: Option<Map<String, Value>>,
     ) -> Result<Proof, Error> {
+        let mut options = options.clone();
+        ensure_or_pick_verification_relationship(&mut options, document, key, resolver).await?;
         // Use type property if present
         let suite = if let Some(ref type_) = options.type_ {
             get_proof_suite(type_)?
@@ -477,8 +479,6 @@ impl LinkedDataProofs {
         else {
             pick_proof_suite(key, options.verification_method.as_ref())?
         };
-        let mut options = options.clone();
-        ensure_or_pick_verification_relationship(&mut options, document, key, resolver).await?;
         suite
             .sign(
                 document,
@@ -501,6 +501,9 @@ impl LinkedDataProofs {
         public_key: &JWK,
         extra_proof_properties: Option<Map<String, Value>>,
     ) -> Result<ProofPreparation, Error> {
+        let mut options = options.clone();
+        ensure_or_pick_verification_relationship(&mut options, document, public_key, resolver)
+            .await?;
         // Use type property if present
         let suite = if let Some(ref type_) = options.type_ {
             get_proof_suite(type_)?
@@ -509,9 +512,6 @@ impl LinkedDataProofs {
         else {
             pick_proof_suite(public_key, options.verification_method.as_ref())?
         };
-        let mut options = options.clone();
-        ensure_or_pick_verification_relationship(&mut options, document, public_key, resolver)
-            .await?;
         suite
             .prepare(
                 document,
