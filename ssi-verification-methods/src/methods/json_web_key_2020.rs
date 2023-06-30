cd4506ab6304b657c1deb20021fa6c1422b1b496
Timothée Haudebourg <timothee.haudebourg@spruceid.com>
2023-06-30 16:36:19 +0200

    Add `ssi-verification-methods` library.

---
+++ ssi-verification-methods/src/methods/json_web_key_2020.rs
@@ -0,0 +1,191 @@
+use std::hash::Hash;
+
+use async_trait::async_trait;
+use iref::{Iri, IriBuf};
+use rdf_types::{literal, Id, Literal, Object, Quad, VocabularyMut};
+use serde::{Deserialize, Serialize};
+use ssi_jwk::JWK;
+use static_iref::iri;
+use treeldr_rust_prelude::{locspan::Meta, AsJsonLdObjectMeta, IntoJsonLdObjectMeta};
+
+use crate::{
+    LinkedDataVerificationMethod, VerificationMethod, CONTROLLER_IRI, RDF_JSON, RDF_TYPE_IRI,
+};
+
+pub const JSON_WEB_KEY_2020_TYPE: &str = "JsonWebKey2020";
+
+pub const JSON_WEB_KEY_2020_IRI: Iri<'static> = iri!("https://w3id.org/security#JsonWebKey2020");
+
+pub const PUBLIC_KEY_JWK_IRI: Iri<'static> = iri!("https://w3id.org/security#publicKeyJwk");
+
+/// JSON Web Key 2020 verification method.
+///
+/// To be used with the [JSON Web Signature 2020][1] cryptographic suite.
+///
+/// See: <https://w3c-ccg.github.io/lds-jws2020/#json-web-key-2020>
+///
+/// [1]: <https://w3c-ccg.github.io/lds-jws2020>
+#[derive(Debug, Clone, PartialEq, Eq, Hash, Serialize, Deserialize)]
+#[serde(tag = "type", rename = "JsonWebKey2020")]
+pub struct JsonWebKey2020 {
+    /// Key identifier.
+    ///
+    /// Should be the JWK thumbprint calculated from the public key value
+    /// according to [RFC7638][rfc7638].
+    ///
+    /// [rfc7638]: <https://w3c-ccg.github.io/lds-jws2020/#bib-rfc7638>
+    id: IriBuf,
+
+    /// Key controller.
+    controller: IriBuf, // TODO: should be an URI.
+
+    /// Public JSON Web Key.
+    #[serde(rename = "publicKeyJwk")]
+    public_key: Box<JWK>,
+}
+
+#[async_trait]
+impl VerificationMethod for JsonWebKey2020 {
+    /// Returns the identifier of the key.
+    fn id(&self) -> Iri {
+        self.id.as_iri()
+    }
+
+    /// Returns the type of the key.
+    fn type_(&self) -> &str {
+        JSON_WEB_KEY_2020_TYPE
+    }
+
+    /// Returns an URI to the key controller.
+    fn controller(&self) -> Iri {
+        self.controller.as_iri()
+    }
+
+    /// Verifies the given signature.
+    async fn verify(
+        &self,
+        controllers: &impl crate::ControllerProvider,
+        proof_purpose: ssi_crypto::ProofPurpose,
+        signing_bytes: &[u8],
+        signature: &[u8],
+    ) -> Result<bool, ssi_crypto::VerificationError> {
+        controllers
+            .ensure_allows_verification_method(
+                self.controller.as_iri(),
+                self.id.as_iri(),
+                proof_purpose,
+            )
+            .await?;
+        match self.public_key.algorithm.as_ref() {
+            Some(a) => {
+                Ok(ssi_jws::verify_bytes(*a, signing_bytes, &self.public_key, signature).is_ok())
+            }
+            None => Err(ssi_crypto::VerificationError::InvalidKey),
+        }
+    }
+}
+
+impl LinkedDataVerificationMethod for JsonWebKey2020 {
+    fn quads(&self, quads: &mut Vec<Quad>) -> Object {
+        quads.push(Quad(
+            Id::Iri(self.id.clone()),
+            RDF_TYPE_IRI.into(),
+            Object::Id(Id::Iri(JSON_WEB_KEY_2020_IRI.into())),
+            None,
+        ));
+
+        quads.push(Quad(
+            Id::Iri(self.id.clone()),
+            CONTROLLER_IRI.into(),
+            Object::Id(Id::Iri(self.controller.clone())),
+            None,
+        ));
+
+        quads.push(Quad(
+            Id::Iri(self.id.clone()),
+            PUBLIC_KEY_JWK_IRI.into(),
+            Object::Literal(Literal::new(
+                serde_json::to_string(&self.public_key).unwrap(),
+                literal::Type::Any(RDF_JSON.into()),
+            )),
+            None,
+        ));
+
+        rdf_types::Object::Id(rdf_types::Id::Iri(self.id.clone()))
+    }
+}
+
+impl<V: VocabularyMut, I, M: Clone> IntoJsonLdObjectMeta<V, I, M> for JsonWebKey2020
+where
+    V::Iri: Eq + Hash,
+    V::BlankId: Eq + Hash,
+{
+    fn into_json_ld_object_meta(
+        self,
+        vocabulary: &mut V,
+        interpretation: &I,
+        meta: M,
+    ) -> json_ld::IndexedObject<V::Iri, V::BlankId, M> {
+        self.as_json_ld_object_meta(vocabulary, interpretation, meta)
+    }
+}
+
+impl<V: VocabularyMut, I, M: Clone> AsJsonLdObjectMeta<V, I, M> for JsonWebKey2020
+where
+    V::Iri: Eq + Hash,
+    V::BlankId: Eq + Hash,
+{
+    fn as_json_ld_object_meta(
+        &self,
+        vocabulary: &mut V,
+        _interpretation: &I,
+        meta: M,
+    ) -> json_ld::IndexedObject<V::Iri, V::BlankId, M> {
+        let mut node = json_ld::Node::with_id(json_ld::syntax::Entry::new(
+            meta.clone(),
+            Meta(
+                json_ld::Id::Valid(Id::Iri(vocabulary.insert(self.id.as_iri()))),
+                meta.clone(),
+            ),
+        ));
+
+        let controller_prop = Meta(
+            json_ld::Id::Valid(Id::Iri(vocabulary.insert(CONTROLLER_IRI))),
+            meta.clone(),
+        );
+        let controller_value = json_ld::Node::with_id(json_ld::syntax::Entry::new(
+            meta.clone(),
+            Meta(
+                json_ld::Id::Valid(Id::Iri(vocabulary.insert(self.controller.as_iri()))),
+                meta.clone(),
+            ),
+        ));
+        node.insert(
+            controller_prop,
+            Meta(
+                json_ld::Indexed::new(json_ld::Object::Node(Box::new(controller_value)), None),
+                meta.clone(),
+            ),
+        );
+
+        let key_prop = Meta(
+            json_ld::Id::Valid(Id::Iri(vocabulary.insert(PUBLIC_KEY_JWK_IRI))),
+            meta.clone(),
+        );
+        let key_value = json_ld::Value::Json(
+            json_syntax::to_value_with(&self.public_key, || meta.clone()).unwrap(),
+        );
+        node.insert(
+            key_prop,
+            Meta(
+                json_ld::Indexed::new(json_ld::Object::Value(key_value), None),
+                meta.clone(),
+            ),
+        );
+
+        Meta(
+            json_ld::Indexed::new(json_ld::Object::Node(Box::new(node)), None),
+            meta,
+        )
+    }
+}
---
+++ ssi-verification-methods/src/reference.rs
@@ -0,0 +1,161 @@
+use core::fmt;
+use std::marker::PhantomData;
+
+use iref::{Iri, IriBuf};
+use rdf_types::VocabularyMut;
+use treeldr_rust_prelude::{locspan::Meta, AsJsonLdObjectMeta, IntoJsonLdObjectMeta};
+
+use crate::LinkedDataVerificationMethod;
+
+/// Reference to a verification method.
+pub struct Reference<M>(IriBuf, PhantomData<M>);
+
+impl<M> Reference<M> {
+    pub fn new(iri: IriBuf) -> Self {
+        Self(iri, PhantomData)
+    }
+
+    pub fn iri(&self) -> Iri {
+        self.0.as_iri()
+    }
+
+    pub fn into_iri(self) -> IriBuf {
+        self.0
+    }
+}
+
+impl<M> fmt::Debug for Reference<M> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        self.0.fmt(f)
+    }
+}
+
+impl<M> Clone for Reference<M> {
+    fn clone(&self) -> Self {
+        Self(self.0.clone(), PhantomData)
+    }
+}
+
+impl<M> PartialEq for Reference<M> {
+    fn eq(&self, other: &Self) -> bool {
+        self.0 == other.0
+    }
+}
+
+impl<M> Eq for Reference<M> {}
+
+impl<M> PartialOrd for Reference<M> {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        self.0.partial_cmp(&other.0)
+    }
+}
+
+impl<M> Ord for Reference<M> {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        self.0.cmp(&other.0)
+    }
+}
+
+impl<M> core::hash::Hash for Reference<M> {
+    fn hash<H: core::hash::Hasher>(&self, state: &mut H) {
+        self.0.hash(state)
+    }
+}
+
+impl<M: LinkedDataVerificationMethod> LinkedDataVerificationMethod for Reference<M> {
+    fn quads(&self, _quads: &mut Vec<rdf_types::Quad>) -> rdf_types::Object {
+        rdf_types::Object::Id(rdf_types::Id::Iri(self.0.clone()))
+    }
+}
+
+/// Reference to a verification method.
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub enum ReferenceOrOwned<M> {
+    Reference(Reference<M>),
+    Owned(M),
+}
+
+impl<M> From<Reference<M>> for ReferenceOrOwned<M> {
+    fn from(value: Reference<M>) -> Self {
+        Self::Reference(value)
+    }
+}
+
+impl<M> From<IriBuf> for ReferenceOrOwned<M> {
+    fn from(value: IriBuf) -> Self {
+        Self::Reference(Reference::new(value))
+    }
+}
+
+impl<M: LinkedDataVerificationMethod> LinkedDataVerificationMethod for ReferenceOrOwned<M> {
+    fn quads(&self, quads: &mut Vec<rdf_types::Quad>) -> rdf_types::Object {
+        match self {
+            Self::Reference(r) => r.quads(quads),
+            Self::Owned(m) => m.quads(quads),
+        }
+    }
+}
+
+impl<V: VocabularyMut, I, M: Clone, T: IntoJsonLdObjectMeta<V, I, M>> IntoJsonLdObjectMeta<V, I, M>
+    for ReferenceOrOwned<T>
+{
+    fn into_json_ld_object_meta(
+        self,
+        vocabulary: &mut V,
+        interpretation: &I,
+        meta: M,
+    ) -> json_ld::IndexedObject<V::Iri, V::BlankId, M> {
+        match self {
+            Self::Reference(r) => Meta(
+                json_ld::Indexed::new(
+                    json_ld::Object::Node(Box::new(json_ld::Node::with_id(
+                        json_ld::syntax::Entry::new(
+                            meta.clone(),
+                            Meta(
+                                json_ld::Id::Valid(json_ld::ValidId::Iri(
+                                    vocabulary.insert_owned(r.into_iri()),
+                                )),
+                                meta.clone(),
+                            ),
+                        ),
+                    ))),
+                    None,
+                ),
+                meta,
+            ),
+            Self::Owned(m) => m.into_json_ld_object_meta(vocabulary, interpretation, meta),
+        }
+    }
+}
+
+impl<V: VocabularyMut, I, M: Clone, T: AsJsonLdObjectMeta<V, I, M>> AsJsonLdObjectMeta<V, I, M>
+    for ReferenceOrOwned<T>
+{
+    fn as_json_ld_object_meta(
+        &self,
+        vocabulary: &mut V,
+        interpretation: &I,
+        meta: M,
+    ) -> json_ld::IndexedObject<V::Iri, V::BlankId, M> {
+        match self {
+            Self::Reference(r) => Meta(
+                json_ld::Indexed::new(
+                    json_ld::Object::Node(Box::new(json_ld::Node::with_id(
+                        json_ld::syntax::Entry::new(
+                            meta.clone(),
+                            Meta(
+                                json_ld::Id::Valid(json_ld::ValidId::Iri(
+                                    vocabulary.insert(r.iri()),
+                                )),
+                                meta.clone(),
+                            ),
+                        ),
+                    ))),
+                    None,
+                ),
+                meta,
+            ),
+            Self::Owned(m) => m.as_json_ld_object_meta(vocabulary, interpretation, meta),
+        }
+    }
+}
