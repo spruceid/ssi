77278f37102201b20583e9370ea97398a4d40e68
Timothée Haudebourg <timothee.haudebourg@spruceid.com>
2023-07-27 11:33:06 +0200

    Add `EcdsaSecp256k1RecoverySignature2020` suite.

--- ssi-vc-ldp/src/suite/dif/ecdsa_secp256k1_recovery_signature_2020.rs
+++ ssi-vc-ldp/src/suite/dif/ecdsa_secp256k1_recovery_signature_2020.rs
@@ -1,4 +1,79 @@
+use ssi_crypto::{SignatureError, Signer, VerificationError, Verifier};
+use ssi_vc::ProofValidity;
+use ssi_verification_methods::EcdsaSecp256k1RecoveryMethod2020;
+use static_iref::iri;
+
+use crate::{
+    impl_rdf_input_urdna2015,
+    suite::{sha256_hash, HashError},
+    verification, CryptographicSuite, ProofConfiguration, ProofOptions, UntypedProof,
+    UntypedProofRef,
+};
+
 /// `EcdsaSecp256k1RecoverySignature2020`.
 /// 
 /// See: <https://identity.foundation/EcdsaSecp256k1RecoverySignature2020/>
 pub struct EcdsaSecp256k1RecoverySignature2020;
+
+impl_rdf_input_urdna2015!(EcdsaSecp256k1RecoverySignature2020);
+
+#[async_trait::async_trait]
+impl CryptographicSuite for EcdsaSecp256k1RecoverySignature2020 {
+    type TransformationParameters = ();
+    type Transformed = String;
+
+    type HashParameters = ProofConfiguration<Self::VerificationMethod>;
+    type Hashed = [u8; 64];
+
+    type ProofParameters = ProofOptions<Self::VerificationMethod>;
+
+    type SigningParameters = ProofOptions<Self::VerificationMethod>;
+
+    type VerificationParameters = ProofOptions<Self::VerificationMethod>;
+
+    type VerificationMethod =
+        verification::MethodReferenceOrOwned<EcdsaSecp256k1RecoveryMethod2020>;
+
+    fn iri(&self) -> iref::Iri {
+        iri!("https://w3id.org/security#EcdsaSecp256k1RecoverySignature2020")
+    }
+
+    fn cryptographic_suite(&self) -> Option<&str> {
+        None
+    }
+
+    fn hash(
+        &self,
+        data: String,
+        proof_configuration: ProofConfiguration<Self::VerificationMethod>,
+    ) -> Result<Self::Hashed, HashError> {
+        Ok(sha256_hash(data.as_bytes(), self, proof_configuration))
+    }
+
+    fn generate_proof(
+        &self,
+        data: &Self::Hashed,
+        signer: &impl Signer<Self::VerificationMethod>,
+        options: ProofOptions<Self::VerificationMethod>,
+    ) -> Result<UntypedProof<Self::VerificationMethod>, SignatureError> {
+        let jws = signer.sign(&options.verification_method, data)?;
+        Ok(UntypedProof::from_options(options, jws.into()))
+    }
+
+    async fn verify_proof(
+        &self,
+        data: &Self::Hashed,
+        verifier: &impl Verifier<Self::VerificationMethod>,
+        proof: UntypedProofRef<'_, Self::VerificationMethod>,
+    ) -> Result<ProofValidity, VerificationError> {
+        let jws = proof
+            .proof_value
+            .as_jws()
+            .ok_or(VerificationError::InvalidProof)?;
+
+        Ok(verifier
+            .verify(proof.verification_method, proof.proof_purpose, data, jws)
+            .await?
+            .into())
+    }
+}
