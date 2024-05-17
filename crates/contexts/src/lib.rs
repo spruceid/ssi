ac74ec9dd9d2b5722887b9cb5850bb4dffc29e12
Jan Christoph Ebersbach <jan-christoph.ebersbach@identinet.io>
2024-05-17 09:12:38 +0200

    chore(contexts): add did-configuration version 1

--- crates/contexts/src/lib.rs
+++ crates/contexts/src/lib.rs
@@ -70,6 +70,8 @@ pub const CACAO_ZCAP_V1: &str = include_str!("../cacao-zcap-v1.jsonld");
 /// <https://w3c-ccg.github.io/vc-ed/plugfest-1-2022/jff-vc-edu-plugfest-1-context.json>
 pub const JFF_VC_EDU_PLUGFEST_2022: &str = include_str!("../jff-vc-edu-plugfest-1-context.json");
 pub const DID_CONFIGURATION_V0_0: &str = include_str!("../did-configuration-v0.0.jsonld");
+/// <https://identity.foundation/.well-known/did-configuration/v1>
+pub const DID_CONFIGURATION_V1: &str = include_str!("../did-configuration-v1.jsonld");
 pub const JFF_VC_EDU_PLUGFEST_2022_2: &str = include_str!("../jff-vc-edu-plugfest-2-context.json");
 
 pub const TZ_V2: &str = include_str!("../tz-2021-v2.jsonld");
