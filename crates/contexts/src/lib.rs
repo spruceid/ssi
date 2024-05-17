9dcd5aa902e0b0e605aecde92f2b1c36f2615a28
Jan Christoph Ebersbach <jan-christoph.ebersbach@identinet.io>
2024-05-17 09:44:08 +0200

    feat(contexts): add linked-vp

--- crates/contexts/src/lib.rs
+++ crates/contexts/src/lib.rs
@@ -71,6 +71,7 @@ pub const CACAO_ZCAP_V1: &str = include_str!("../cacao-zcap-v1.jsonld");
 pub const JFF_VC_EDU_PLUGFEST_2022: &str = include_str!("../jff-vc-edu-plugfest-1-context.json");
 pub const DID_CONFIGURATION_V0_0: &str = include_str!("../did-configuration-v0.0.jsonld");
 pub const JFF_VC_EDU_PLUGFEST_2022_2: &str = include_str!("../jff-vc-edu-plugfest-2-context.json");
+pub const LINKED_VP_V1: &str = include_str!("../linked-vp-v1.jsonld");
 
 pub const TZ_V2: &str = include_str!("../tz-2021-v2.jsonld");
 pub const TZVM_V1: &str = include_str!("../tzvm-2021-v1.jsonld");
