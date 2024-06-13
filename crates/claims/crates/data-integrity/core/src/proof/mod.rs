bf6a6a04f4ff74c06c3aeaa1bd57853ce96ed56d
Timothée Haudebourg <timothee.haudebourg@spruceid.com>
2024-06-13 12:12:55 +0200

    Successful bbs-2023 verification.

--- crates/claims/crates/data-integrity/core/src/proof/mod.rs
+++ crates/claims/crates/data-integrity/core/src/proof/mod.rs
@@ -392,7 +392,6 @@ impl<'de, S: DeserializeCryptographicSuite<'de>> Deserialize<'de> for Proofs<S> 
     where
         D: serde::Deserializer<'de>,
     {
-        eprintln!("start deserializing proofs");
         match OneOrMany::<Proof<S>>::deserialize(deserializer)? {
             OneOrMany::One(proof) => Ok(Self(vec![proof])),
             OneOrMany::Many(proofs) => Ok(Self(proofs)),
