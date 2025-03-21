1aa4ba27a5821a6a585acb7e3e5e30b84661e513
Timothée Haudebourg <timothee.haudebourg@spruceid.com>
2025-03-21 17:47:53 +0100

    Avoid some `unwrap`-ing.

--- crates/crypto/src/options.rs
+++ crates/crypto/src/options.rs
@@ -6,26 +6,26 @@ use std::{
 
 /// Arbitrary signing or verification options.
 ///
-/// Some cryptographic schemes require optional parameters that can be provided
+/// Some cryptographic schemes have optional parameters that can be provided
 /// using this type.
 #[derive(Debug, Default)]
 pub struct Options(HashMap<TypeId, Box<dyn Send + Sync + Any>>);
 
 impl Options {
     pub fn get<T: 'static + Send + Sync>(&self) -> Option<&T> {
-        self.0.get(&TypeId::of::<T>()).map(downcast_ref)
+        self.0.get(&TypeId::of::<T>()).and_then(downcast_ref)
     }
 
     pub fn get_or_default<T: 'static + Default + Clone + Send + Sync>(&self) -> Cow<T> {
         self.0
             .get(&TypeId::of::<T>())
-            .map(downcast_ref)
+            .and_then(downcast_ref)
             .map(Cow::Borrowed)
             .unwrap_or_else(|| Cow::Owned(T::default()))
     }
 
     pub fn get_mut<T: 'static + Send + Sync>(&mut self) -> Option<&mut T> {
-        self.0.get_mut(&TypeId::of::<T>()).map(downcast_mut)
+        self.0.get_mut(&TypeId::of::<T>()).and_then(downcast_mut)
     }
 
     pub fn insert<T: 'static + Send + Sync>(&mut self, value: T) -> Option<T> {
@@ -44,11 +44,11 @@ fn downcast<T: 'static>(t: Box<dyn Send + Sync + Any>) -> T {
 }
 
 #[allow(clippy::borrowed_box)]
-fn downcast_ref<T: 'static>(t: &Box<dyn Send + Sync + Any>) -> &T {
-    t.downcast_ref().unwrap()
+fn downcast_ref<T: 'static>(t: &Box<dyn Send + Sync + Any>) -> Option<&T> {
+    t.downcast_ref()
 }
 
 #[allow(clippy::borrowed_box)]
-fn downcast_mut<T: 'static>(t: &mut Box<dyn Send + Sync + Any>) -> &mut T {
-    t.downcast_mut().unwrap()
+fn downcast_mut<T: 'static>(t: &mut Box<dyn Send + Sync + Any>) -> Option<&mut T> {
+    t.downcast_mut()
 }
